502ed97989eed1b3a97976053353550f0590823b
Tanmay s ail <142146589+HellTanmay@users.noreply.github.com>
2025-08-24 20:21:02 +0530

    Changed some designs

--- src/ProductList.jsx
+++ src/ProductList.jsx
@@ -226,12 +226,6 @@ function ProductList({ onHomeClick }) {
         alignIems: 'center',
         fontSize: '20px',
     }
-    const styleObjUl = {
-        display: 'flex',
-        justifyContent: 'space-between',
-        alignItems: 'center',
-        width: '1100px',
-    }
     const styleA = {
         color: 'white',
         fontSize: '30px',
@@ -285,12 +279,12 @@ function ProductList({ onHomeClick }) {
                     </div>
 
                 </div>
-                <div style={styleObjUl}>
-                    <div> <a href="#" onClick={(e) => handlePlantsClick(e)} style={styleA}>Plants</a></div>
+                {/* <div style={styleObjUl}> */}
+                    <div style={{marginTop:"20px", marginRight:"100px"}}> <a href="#" onClick={(e) => handlePlantsClick(e)} style={styleA}>Plants</a></div>
                     <div> <a href="#" onClick={(e) => handleCartClick(e)} style={styleA}><h1 className='cart'><svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 256 256" id="IconChangeColor" height="68" width="68"><rect width="156" height="156" fill="none" ></rect>
                     <text x="130" y="130" textAnchor="middle" dominantBaseline="middle" fill="white" fontSize="60" fontWeight="bold">{Products.length}</text><circle cx="80" cy="216" r="12"></circle><circle cx="184" cy="216" r="12"></circle><path d="M42.3,72H221.7l-26.4,92.4A15.9,15.9,0,0,1,179.9,176H84.1a15.9,15.9,0,0,1-15.4-11.6L32.5,37.8A8,8,0,0,0,24.8,32H8" fill="none" stroke="#faf9f9" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" id="mainIconPathAttribute"></path>
                    </svg></h1></a></div>
-                </div>
+                {/* </div> */}
             </div>
             {!showCart ? (
                 <div className="product-grid">
